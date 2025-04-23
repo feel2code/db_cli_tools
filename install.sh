9ca93c8a411dbeed60979573576d71d4281a01aa
Feliks Nabiullin <fnabiullin@fxcintel.com>
2025-04-23 20:30:01 +0300

    installation guide added

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OS_NAME=$(uname)
+
+if [[ "$OS_NAME" == "Darwin" || "$OS_NAME" == "Linux" ]]; then
+
+  curl https://clickhouse.com/ | sh
+  sudo ./clickhouse install
+  pipx install mycli
+  pipx install pgcli
+  # for mongosh install please visit
+  # https://www.mongodb.com/docs/mongodb-shell/install/
+
+else
+  curl https://clickhouse.com/ | sh
+  sudo ./clickhouse install
+  brew install pgcli mycli mongosh
+
+fi
